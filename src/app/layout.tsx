108fbabfeb8f4c0f7056dcc7e6855968836c92c3
Дарья Капусто <daria.k.searchindustrial@gmail.com>
2025-09-04 14:03:48 +0000

    Шрифт – Onest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from 'next';
-import { PT_Sans } from 'next/font/google';
+import { Onest } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 
-const ptSans = PT_Sans({
+const onest = Onest({
   subsets: ['latin', 'cyrillic'],
   weight: ['400', '700'],
-  variable: '--font-pt-sans',
+  variable: '--font-onest',
 });
 
 export const metadata: Metadata = {
@@ -25,9 +25,9 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Onest:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased', ptSans.variable)}>
+      <body className={cn('font-body antialiased', onest.variable)}>
         {children}
         <Toaster />
       </body>
