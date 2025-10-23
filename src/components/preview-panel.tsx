5ffddb1fef3028f32680bf897372030b1ceb2798
Дарья Капусто <daria.k.searchindustrial@gmail.com>
2025-10-23 13:52:11 +0000

    Убери паттерн из документа

--- src/components/preview-panel.tsx
+++ src/components/preview-panel.tsx
@@ -61,24 +61,8 @@ export default function PreviewPanel({ proposal, branding }: PreviewPanelProps) 
     return elements;
   };
   
-  const imagePattern = `
-    <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <defs>
-        <pattern id="logoPattern" patternUnits="userSpaceOnUse" width="21cm" height="29.7cm">
-          <image xlink:href="https://searchindustrial.ru/img/Logo_dark.png" x="-1.25cm" y="2cm" width="2.5cm" height="2.5cm" opacity="0.5" />
-          <image xlink:href="https://searchindustrial.ru/img/Logo_dark.png" x="18cm" y="5cm" width="4.5cm" height="4.5cm" opacity="0.5" />
-          <image xlink:href="https://searchindustrial.ru/img/Logo_dark.png" x="2cm" y="12cm" width="2.5cm" height="2.5cm" opacity="0.5" />
-          <image xlink:href="https://searchindustrial.ru/img/Logo_dark.png" x="15cm" y="18cm" width="4.5cm" height="4.5cm" opacity="0.5" />
-          <image xlink:href="https://searchindustrial.ru/img/Logo_dark.png" x="-0.5cm" y="24cm" width="2.5cm" height="2.5cm" opacity="0.5" />
-          <image xlink:href="https://searchindustrial.ru/img/Logo_dark.png" x="19cm" y="28cm" width="4.5cm" height="4.5cm" opacity="0.5" />
-        </pattern>
-      </defs>
-      <rect width="100%" height="100%" fill="url(#logoPattern)"/>
-    </svg>
-  `;
-
   const cardStyle = {
-    backgroundImage: branding.backgroundUrl ? `url(${branding.backgroundUrl})` : `url("data:image/svg+xml,${encodeURIComponent(imagePattern)}")`,
+    backgroundImage: branding.backgroundUrl ? `url(${branding.backgroundUrl})` : undefined,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   };
