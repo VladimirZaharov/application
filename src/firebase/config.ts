ec6f70485367fc11b9088ea06bc6f45e440b55f1
Дарья Капусто <daria.k.searchindustrial@gmail.com>
2025-10-30 09:22:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "kp2-29536267-bd7d9",
+  "appId": "1:108684364249:web:db4852d7429537bfce31c8",
+  "apiKey": "AIzaSyCFSXRCXAysK5PXSBf46S0ucB57EVJQPnE",
+  "authDomain": "kp2-29536267-bd7d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "108684364249"
+};
